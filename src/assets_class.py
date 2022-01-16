d7a1a2fa788e5e47e6382102267246955d0c395c
syed-hussain-93 <syed.musharot.hussain@gmail.com>
2022-01-16 15:59:29 +0000

    started streamlit web app

--- src/assets_class.py
+++ src/assets_class.py
@@ -1,33 +1,31 @@
 from typing import Dict
 from pandas_datareader import data as web
 import pandas as pd
-from datetime import datetime
-from dateutil import relativedelta
-
 import numpy as np
 
 
 TRADING_DAYS_PER_YEAR = 250
 
+
 class Asset:
-    
     def __init__(self, stock: dict):
-        
+
         self.name = stock["name"]
         self.data = stock["data"]
-                
+
         # Daily log returns
-        self.daily_returns = self.data['Close'].pct_change().apply(lambda x: np.log(1+x)).dropna()
+        self.daily_returns = (
+            self.data["Close"].pct_change().apply(lambda x: np.log(1 + x)).fillna(0)
+        )
         # Expected daily returns
         self.expected_daily_return = np.mean(self.daily_returns)
-        
+
         # self.data = pd.DataFrame()
         # self.data[self.name] = web.DataReader(self.name, data_source='yahoo', start= self.start_date, end= self.end_date)['Close']
-    
+
     def annual_returns(self) -> pd.DataFrame:
-        
+
         return self.expected_daily_return * TRADING_DAYS_PER_YEAR
-    
+
     def __repr__(self):
-        return f'<Asset name={self.name}, expected return={self.expected_daily_return}>'
-    
+        return f"<Asset name={self.name}, expected return={self.expected_daily_return}>"
--- src/main.py
+++ src/main.py
@@ -1,15 +1,39 @@
+import streamlit as st
+import yfinance as yf
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
 from assets_class import Asset
 from portfolio_class import Portfolio
-from datetime import datetime
-import numpy as np
-import pandas as pd
-date_range = ["2013-01-01", datetime.today().strftime('%Y-%m-%d')]
-ticker_list = ["AAPL", "FB", "AMZN", "NFLX", "GOOG"]
 
-# portfolio = Portfolio([Asset({"ticker": ticker, "date_range": date_range}) for ticker in ticker_list])
-portfolio = Portfolio(ticker_list, date_range=date_range)
 
-portfolio_dataframe = portfolio.get_dataframe()
+def yf_retrieve_data(ticker_list, date_range):
+    data_frames = []
+    for ticker in ticker_list:
+        data = yf.download(ticker, start=date_range[0], end=date_range[1])
+        data_frames.append(data)
+    return data_frames
 
-test_asset = Asset({"ticker": "FB", "date_range": date_range})
 
+def main():
+    st.title("Portfolio Optimiser")
+
+    menu = ["Home", "Explore"]
+    choice = st.sidebar.selectbox("Select Page", menu)
+
+    if choice == "Home":
+        st.subheader("Optimise Portfolio")
+
+        ticker_list = ("AAPL", "FB", "AMZN", "NFLX", "GOOG")
+        dropdown = st.multiselect("Pick your assets", ticker_list)
+
+        start = st.date_input("Start", value=pd.to_datetime("2010-01-01"))
+        end = st.date_input("End", value=pd.to_datetime("today"))
+
+    else:
+        st.subheader("Explore")
+
+
+if __name__ == "__main__":
+    main()
